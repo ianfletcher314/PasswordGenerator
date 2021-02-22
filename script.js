9938efd1c56d2a0fd717b8e1576164b43b601e4c
Ian Fletcher <ianfletcher314@gmail.com>
2021-02-22 16:58:56 -0600

    2nd commit with updated parameters that dont allow strings for the prompt and make sure that at least one category of character is selected.

--- script.js
+++ script.js
@@ -1,60 +1,82 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// generatePassword(); 
-// Write password to the #password input
-
-/*Prompts and comfirms that are set to variables. take the variables and make if then statements creating a library of
-potnetial characters housed 
-in a LibararyVar using concat to combine strings . then take a random number generator function(for loop) that runs N 
-number of times n being one of the initial promp vars. this number 
-generator functoin will generate a number (between 1 and length of Library Var) refference what number of character  that 
-number is in the libraryVar (libraryVar[1]) and add it to the Password var-
-this then repeats and grabs another letter and then adds it to the password var over and over the number of times the person dicatated */
-
 
 function writePassword() {
-  // 0) CharacterLibrary = [alphabet]
+  // 1) Set libraries with special characters, lower case, upper case, and numbers. Make an empty master library that you will add the other librarys to
+  
   var generatePassword = []
-  var MasterLibrary = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var MasterLibrary = [];
   var CharacterLibrary = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var SpecLibrary = ['!','@','#','$','%','^','&','*','?'];
   var NumLibrary = ['1','2','3','4','5','6','7','8','9','0'];
   var CapLibrary = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',"'Z"]
-  // 1) confirm for number characters
-  var numberCharacters = window.confirm("If you want numbers in the password press 'ok' otherwise press cancel!")
-  console.log(numberCharacters)
-  // 2) confirm for capital characters
-  var specialCharacters = window.confirm("If you want special characters in the password press 'ok' otherwise press cancel!")
-  console.log(specialCharacters)
-  // 3) confirm for special characters 
-  var CapitalCharacters = window.confirm("If you want capital letters in the password press 'ok' otherwise press cancel!")
-  console.log(CapitalCharacters)
-  // 4) prompt for password length 
+  var didUserSelectAtLeastOneCritera = false
+
+  console.log(MasterLibrary)
+  // 2) prompt for password length 
   var PasswordPromptLength = window.prompt("How many characters do you want your password to be?")
   console.log(PasswordPromptLength)
-  // 5) if the answer is bellow 8 OR above 128 reprompt until a number in that range is chosen
+  // 3) if the answer is bellow 8 OR above 128 reprompt until a number in that range is chosen
   while (PasswordPromptLength < 8 || PasswordPromptLength > 128) {
     var PasswordPromptLength = window.prompt("So Sorry! The password must be between 8 and 128 Characters. How long do you want your password to be?")
     console.log(PasswordPromptLength)
   }
-  // 6) if capital letters confirm === true >>> add [CapLibarary] to CharacterLibrary
-  if (CapitalCharacters === true) {
-    var MasterLibrary = CharacterLibrary.concat(CapLibrary)
+  // 4) If the input the user has entered is not a number make them re-start writePassword function
+  if (isNaN(parseInt(PasswordPromptLength))) {
+		alert('Sorry, you must input a number')
+		writePassword()
+		return
+	} 
+  // 5) confirm for lower case letters characters
+  var lowerCharacters = window.confirm("If you want lowercase letters in the password press 'ok' otherwise press cancel!")
+  console.log(lowerCharacters, "are lower characters")
+
+  // 6) confirm for number characters
+  var numberCharacters = window.confirm("If you want numbers in the password press 'ok' otherwise press cancel!")
+  console.log(numberCharacters)
+  // 7) confirm for special characters
+  var specialCharacters = window.confirm("If you want special characters in the password press 'ok' otherwise press cancel!")
+  console.log(specialCharacters)
+  // 8) confirm for Capital letters 
+  var CapitalCharacters = window.confirm("If you want capital letters in the password press 'ok' otherwise press cancel!")
+  console.log(CapitalCharacters)
+
+  // 9) if lower letters confirm === true >>then>> add [characterLibarary] to MasterLibrary
+  if (lowerCharacters === true) {
+    var MasterLibrary = CharacterLibrary.concat(CharacterLibrary)
+    var didUserSelectAtLeastOneCritera = true
   }
   console.log(MasterLibrary)
 
-  // 7) if special characters confirm === true >>> add [SpecialCharacters] to CharacterLibrary
+  // 10) if capital letters confirm === true >>then>> add [CapLibarary] to CharacterLibrary
+  if (CapitalCharacters === true) {
+    var MasterLibrary = MasterLibrary.concat(CapLibrary)
+    var didUserSelectAtLeastOneCritera = true
+  }
+  console.log(MasterLibrary)
+
+  // 11) if special characters confirm === true >>then>> add [SpecialCharacters] to CharacterLibrary
   if (specialCharacters === true) {
     var MasterLibrary = MasterLibrary.concat(SpecLibrary)
+    var didUserSelectAtLeastOneCritera = true
   }
   console.log(MasterLibrary)
-  // 8) if number characters confirm === true >>> add [numberCharacters] to CharacterLibrary
+
+  // 12) if number characters confirm === true >>then>> add [numberCharacters] to CharacterLibrary
   if (numberCharacters === true) {
     var MasterLibrary = MasterLibrary.concat(NumLibrary)
+    var didUserSelectAtLeastOneCritera = true
   }
   console.log(MasterLibrary)
-  // 9) for loop random number generator function that finds a number between 0 and [length.CharacterLibrary],then figures out what character
-  // in the character library is positioned at that number ie: CharacterLibrary[randomnumber], 
+
+  // 13) If use did not select numbers letters or special symbols make them re start 
+  if (!didUserSelectAtLeastOneCritera){
+		alert('you must have lower case letters, upper case letters, numbers, or special characters. Please select one!')
+		writePassword()
+		return
+	}
+  // 14) for loop random number generator function that finds a number between 0 and [length.CharacterLibrary],then figures out what character
+  // in the Masterlibrary is positioned at that number ie: MasterLibrary[randomnumber], 
   // and then add the characte to the password string (probably using concat), this is repeated N number of times!
   console.log(PasswordPromptLength, "is the PPL")
   for (i=0; i < PasswordPromptLength;i++){
@@ -69,7 +91,6 @@ function writePassword() {
   var password = generatePassword
   var passwordText = document.querySelector("#password");
   
-  // password = "ian is cool";
   passwordText.value = password.join("");
 }
 
@@ -78,12 +99,3 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
-// if (PasswordPromptLength < 129 && PasswordPromptLength > 7) {
-//   var PasswordLength = PasswordPromptLength
-//   console.log("the password length is" , PasswordLength )
-// }
-// else {
-// var PasswordPromptLength = window.prompt("So Sorry! The password must be between 8 and 128 Characters. How long do you want your password to be?")
-// console.log(PasswordLength)
-// }
